5615839944b4c8180df541dcc931e125466022f5
lositskyi <lositskyi@gmail.com>
2018-03-29 00:39:49 +0300

    Add Lesson 3

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PostList from './Components/PostList.js';
+
+class App extends Component {
+  render() {
+
+    return (
+      < PostList />
+    );
+  }
+}
+
+export default App;
