a5a771479d2f4c7d1759e666e1e62c77aafa3cd4
lositskyi <lositskyi@gmail.com>
2018-04-05 00:46:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+class InvalidArgumentError extends Error {
+	constructor(argument, value) {
+		super();
+		this.name = "InvalidArgument";
+		this.message = `Invalid value of the argument ${argument}! Argument can not be ${value}`;
+		this.stack = (new Error()).stack;
+	}
+}
 
 const React = {
 	createElement (type, props, children) {
-		//varification argument "type"
 		if (typeof type !== 'string') {
-			throw new Error(`Invalid value of the argument "type" in createElement()! "type" is a ${typeof type}. Argument must be a string. ""`);
+			throw new InvalidArgumentError('type', type);
 		}
 		
 		if (typeof props !== 'object' && typeof props !== 'undefined') {
-			throw new Error(`Invalid value of the argument "props" in createElement()! Argument must be an object.`);
+			throw new InvalidArgumentError('props', props);
 		}
 		
 		const element = document.createElement(type);
@@ -15,9 +22,13 @@ const React = {
 		for (let key in props) {
 		  switch (key) {
 			case 'style':
-			  for (let prop in props[key]) {
-				element.style[prop] = props.style[prop];
-			  };
+			  if(typeof props[key] == 'string') {
+				element.style.cssText = props[key];
+			  } else if (!Array.isArray(props[key] && !props[key])){
+				for (let prop in props[key]) {
+					element.style[prop] = props.style[prop];
+				}
+			  }
 			  break;
 			case 'cssText':
 			  element.style.cssText = props.cssText;
@@ -27,6 +38,7 @@ const React = {
 			  break;
 			default:
 			  element[key] = props[key];
+			  break;
 		  }
 		}
 		
@@ -50,8 +62,11 @@ const React = {
 					} else if (children.nodeType){
 						element.appendChild(children);						
 					} else {
-						throw new Error(`Invalid value of the argument "children" in createElement()! Argument "children" must be an array or Node element.`);
+						throw new InvalidArgumentError('children', children)
 					} 
+					break;
+				default:
+					throw new InvalidArgumentError('children', children);
 					break;
 			}
 		}
@@ -59,27 +74,29 @@ const React = {
 	  },
 
 	  render(element, parentElement) {
-		if (!parentElement) throw new Error(`Invalid value of the argument "parentElement" in render()! "parentElement" is null`);
-		if (!parentElement || !parentElement.nodeType) throw new Error(`Invalid value of the argument "parentElement" in render()! "parentElement" must be a Node element `);
+		if (!parentElement || !parentElement.nodeType) {
+			throw new InvalidArgumentError('parentElement', parentElement);
+		}
 		
 		if (typeof element == 'string') {
 			parentElement.innerHTML = element;
 		} else if (element.nodeType) {
 			parentElement.innerHTML = element.outerHTML;
+		} else {
+			throw new InvalidArgumentError('element', element);
 		}
 	  }
 	}
 
-	const app = 
-	  React.createElement('div', { style: { backgroundColor: 'red' } }, [
-	    React.createElement('span', undefined, 'Hello world'),
-	    React.createElement('br'),
-	    'This is just a text node',
-	    React.createElement('div', { textContent: 'Text content' })
-	  ]);
+const app = 
+	React.createElement('div', { style: { backgroundColor: 'red' } }, [
+	   React.createElement('span', undefined, 'Hello world'),
+	   React.createElement('br'),
+	   'This is just a text node',
+	   React.createElement('div', { textContent: 'Text content' })
+	]);
 
 React.render(
 	app,
 	document.getElementById('root'),
 );
-
